e3e5b3dd3b4b752c8b848a58b4dd3e7a0a02489c
Joseph <josephmacdonald4629@gmail.com>
2023-11-28 19:51:36 -0400

    Add code to load map from file and display it

--- TextBased(RPG)/Program.cs
+++ TextBased(RPG)/Program.cs
@@ -9,10 +9,44 @@ namespace TextBased_RPG_
 {
     internal class Program
     {
+        static char[,] map; // Map properties
+        static int mapHeight; 
+        static int mapWidth;
 
-        static void Main(string[] args)
+        static void Main(string[] args) // Main 
         {
+            LoadMap("mapArea.txt");
+            DisplayMap();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
 
+        static void LoadMap(string fileName) // Load the map text from the map file
+        {
+            string[] lines = File.ReadAllLines(fileName);
+            mapHeight = lines.Length;
+            mapWidth = lines[0].Length;
+            map = new char[mapHeight, mapWidth];
+
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    map[i, j] = lines[i][j];
+                }
+            }
+        }
+
+        static void DisplayMap() // Display the map text
+        {
+            for (int i = 0; i < mapHeight; i++)
+            {
+                for (int j = 0; j < mapWidth; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
