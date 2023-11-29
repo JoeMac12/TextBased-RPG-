4b441cc2cafa6d0a9dc6b6a60ee2dfcf9088e946
Joseph <josephmacdonald4629@gmail.com>
2023-11-28 20:57:11 -0400

    Add basic support for player movement

--- TextBased(RPG)/Program.cs
+++ TextBased(RPG)/Program.cs
@@ -19,9 +19,10 @@ namespace TextBased_RPG_
             LoadMap("mapArea.txt");
             InitializePlayer();
 
-            while (true) // Basic support for updating the map for game inputs
+            while (true) // Creats a new map every input. I'll try and fix that later to make it just the one map
             {
                 DisplayMap();
+                PlayerMovement();
             }
 
             //Console.WriteLine("Press any key to exit");
@@ -69,6 +70,36 @@ namespace TextBased_RPG_
         static void InitializePlayer() // Starting spot of the player, maybe add random starting spot later?
         {
             playerPosition = (1, 1);
+        }
+
+        static void PlayerMovement() // Controls for the player movement 
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.W:
+                    MovePlayer(0, -1); // Up 1 unit
+                    break;
+                case ConsoleKey.S:
+                    MovePlayer(0, 1); // Down 1 unit
+                    break;
+                case ConsoleKey.A:
+                    MovePlayer(-1, 0); // Left 1 unit
+                    break;
+                case ConsoleKey.D:
+                    MovePlayer(1, 0); // Right 1 unit
+                    break;
+            }
+        }
+
+        static void MovePlayer(int x, int y) // Handles where the player moves to on the map
+        {
+            int moveX = playerPosition.x + x;
+            int moveY = playerPosition.y + y;
+            if (moveX >= 0 && moveX < mapWidth && moveY >= 0 && moveY < mapHeight && map[moveY, moveX] != '#') // Check if it's a wall
+            {
+                playerPosition = (moveX, moveY); // Move the player to the new position
+            }
         }
 
         static void SetTextColor(char textType) // Color for each text type
